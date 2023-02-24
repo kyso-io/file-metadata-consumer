a41886afb2d7770c92a97205f2ae6982c3bddcc2
Fran Barrena <francisco@kyso.io>
2023-02-24 19:23:52 +0100

    fix: using env variables instead of .env files

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
+import os
 from dotenv import dotenv_values
 from pymongo import MongoClient
 
-config = dotenv_values()
+config = os.environ["DATABASE_URI"]
 
-if "DATABASE_URI" not in config:
-    raise Exception("DATABASE_URI not found in .env file.")
+if not config:
+    raise Exception("DATABASE_URI not found")
 
 client = MongoClient(config["DATABASE_URI"])
 db = client["kyso"]
